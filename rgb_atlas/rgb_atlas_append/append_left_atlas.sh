fa981bfce65a8da2c2c7816b2fd7d9badb72edd7
muyezhu <muyezhu@usc.edu>
2015-11-19 12:30:40 -0800

    fix executable calling path

--- rgb_atlas/rgb_atlas_append/append_left_atlas.sh
+++ rgb_atlas/rgb_atlas_append/append_left_atlas.sh
@@ -3,13 +3,14 @@ DIR='/ifs/loni/faculty/dong/mcp/muye/projects/rgb_atlas/rgb_atlas_append/'
 NEWDIR='/ifs/loni/faculty/dong/mcp/muye/rgb_atlas_test/'
 CONVERT='/usr/local/ImageMagick-6.7.8-2_64bit/bin/convert'
 MOGRIFY='/usr/local/ImageMagick-6.7.8-2_64bit/bin/mogrify'
+READOFFSET=$DIR"read_offset"
 for img in $DIR*"rgb"*"tif";
 do
     imgroll=${img%%.*}'-roll.tif'
 	imgshave=${imgroll%%.*}'-shave.tif'
     imgflop=${imgshave%%.*}'-flop.tif'
 	imgappendleft=${img%%.*}'-append.tif'
-	./read_offset $img
+	$READOFFSET $img
 	val=$(cat "offset.txt")
 	# roll
 	$CONVERT -roll +$val+0 $img $imgroll
